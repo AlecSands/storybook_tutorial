a228bd76382866061ce77f6a5953b7ec92b6ec14
Alec Sands <sandsalecv@gmail.com>
2020-07-02 12:19:11 -0500

    adding Apollo with basic graphql query

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
+client.query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  }).then(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
